a764b7cd6711d973f63ea4d5b55e6000ffc8f279
naDuong2004 <nguyenanhduong112004@gmail.com>
2024-10-12 22:15:19 +0700

    .

--- src/scripts/test.js
+++
@@ -1,9 +0,0 @@
-const testFunction = async () => {
-  const response = await fetch(
-    "https://www.themealdb.com/api/json/v1/1/list.php?i=list"
-  );
-  const data = await response.json();
-  console.log(data.meals[0]);
-};
-
-testFunction();
