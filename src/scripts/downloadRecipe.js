c62ebf9753aff86372560c2a317a7afc8f82dca6
DUYCUTEPHOMAIQUE <dauhu1232019@gmail.com>
2024-10-13 18:08:29 +0700

    duy push trc khi pull duong

--- src/scripts/downloadRecipe.js
+++ src/scripts/downloadRecipe.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 const mongoose = require('mongoose');
 const recipeModel = require('../models/recipe.model');
+const { forEach } = require('lodash');
 
 const API_URL = 'https://www.themealdb.com/api/json/v1/1/random.php';
 const RAPID_API_KEY = 'ad4264eb0cmsh25d2f0eb8ff8eebp12f991jsn844061a51936'
@@ -21,89 +22,81 @@ async function main() {
         console.log(`[t]==${t}`)
         t++;
         try {
-            const response = await axios.get(API_URL);
-            const data = response.data.meals[0]
-            // console.log(data)
-            let ingredients = []
-            for(let i = 1; i <= 15; i++) {
-                let ingredient = data[`strIngredient${i}`];
-                let measure = data[`strMeasure${i}`];
             
-                // Check if both ingredient and measure exist
-                if (ingredient && measure) {
-                    let temp = {
-                        name: ingredient,
-                        measure: measure
+            const response = await recipeModel.find()
+            // console.log(response[0])
+
+            for(let i = 0; i < response.length; ++i) {
+                let text = response[i].recipe_name + " | " + response[0].recipe_desciption ;
+                let recipe_ingredients = response[i].recipe_ingredients;
+                console.log(recipe_ingredients)
+                recipe_ingredients.forEach((item, index) => {
+                    text+= " | "+item.name
+                })
+
+                console.log(text)
+        
+                try {
+                    const options = {
+                        method: 'POST',
+                        url: 'https://google-translate113.p.rapidapi.com/api/v1/translator/text',
+                        headers: {
+                            'x-rapidapi-key': key,
+                            'x-rapidapi-host': 'google-translate113.p.rapidapi.com',
+                            'Content-Type': 'application/json'
+                        },
+                        data: {
+                            from: 'en',
+                            to: 'vi',
+                            text: text // Gửi toàn bộ mảng text cần dịch
+                        }
                     };
-                    ingredients.push(temp);
+                    const responseOK = await axios.request(options);
+                    const translatedTexts = responseOK.data['trans'];
+                    console.log("==>", translatedTexts)
+                    const temp = translatedTexts.split("|")
+                    const tempIngre = temp.slice(2, temp.length+1)
+                    console.log(temp.slice(2, temp.length+1));
+                    for(let i = 0; i < tempIngre.length; i++) {
+                        recipe_ingredients[i].name = tempIngre[i];
+                    }
+                    console.log(recipe_ingredients)
+                    
+                    try {
+                        // ... (rest of the translation code remains the same)
+                    
+                        const query = {_id: response[i]._id}
+                        const updateSet = {
+                            recipe_name: temp[0],
+                            recipe_ingredients: recipe_ingredients,
+                        }
+
+    
+                        const updateRecipe = await recipeModel.findOneAndUpdate(query, updateSet, {new: true})
+                        if(!updateRecipe) {
+                            console.log("Error updating recipe!")
+                        } else {
+                            console.log("Updated recipe:", updateRecipe)
+                        }
+    
+                    
+                        
+                    } catch (error) {
+                        console.error("Error in translation or update:", error)
+                    }
+                    
+                } catch (error) {
+                    
                 }
-            } 
-            // console.log(ingredients)
-            const text = data['strInstructions'];
-            // const options = {
-            //     method: 'POST',
-            //     url: 'https://google-api31.p.rapidapi.com/translate',
-            //     headers: {
-            //       'x-rapidapi-key': key,
-            //       'x-rapidapi-host': 'google-api31.p.rapidapi.com',
-            //       'Content-Type': 'application/json'
-            //     },
-            //     data: {
-            //         text: text,
-            //       to: 'vi',
-            //       from_lang: ''
-            //     }
-            //   };
+            }
 
-              
-            // const options = {
-            //     method: 'POST',
-            //     url: 'https://google-translate113.p.rapidapi.com/api/v1/translator/text',
-            //     headers: {
-            //     'x-rapidapi-key': key,
-            //     'x-rapidapi-host': 'google-translate113.p.rapidapi.com',
-            //     'Content-Type': 'application/json'
-            //     },
-            //     data: {
-            //     from: 'auto',
-            //     to: 'vi',
-            //     text: text
-            //     }
-            // };
 
-            // // const {rsOK} = await axios.post('https://libretranslate.com/translate', {
-            // //     q: text,
-            // //     source: 'en',
-            // //     target: 'vi',
-            // //     format: 'text'
-            // // });  
-            // const res = await axios.request(options)
-            // console.log(res.data['trans'])
-            // const translated = res.data['trans']
 
-            //TODO
-            // Chuẩn bị các chuỗi cần dịch
-            const textToTranslate = [
-                data['strInstructions'],  // Dịch instructions
-                ...ingredients.map(item => item.name),  // Dịch tất cả tên ingredients
-                ...ingredients.map(item => item.measure)  // Dịch tất cả measure
-            ];
+            // console.log(data)
+            
 
             // Gửi yêu cầu dịch
-            const options = {
-                method: 'POST',
-                url: 'https://google-translate113.p.rapidapi.com/api/v1/translator/text',
-                headers: {
-                    'x-rapidapi-key': RAPID_API_KEY,
-                    'x-rapidapi-host': 'google-translate113.p.rapidapi.com',
-                    'Content-Type': 'application/json'
-                },
-                data: {
-                    from: 'auto',
-                    to: 'vi',
-                    text: textToTranslate // Gửi toàn bộ mảng text cần dịch
-                }
-            };
+         
 
             // const options = {
             //     method: 'POST',
@@ -123,40 +116,40 @@ async function main() {
             //     }
             //   };
 
-            const res = await axios.request(options);
+            // const res = await axios.request(options);
 
-            // Kết quả dịch
-            const translatedTexts = res.data['translation'];
-            console.log(translatedTexts)
+            // // Kết quả dịch
+            // const translatedTexts = res.data['trans'];
+            // console.log(translatedTexts)
 
-            // Ánh xạ kết quả dịch lại vào đúng chỗ
-            const translatedInstructions = translatedTexts[0];  // Bản dịch của `strInstructions`
-            const translatedIngredients = ingredients.map((item, index) => ({
-                name: translatedTexts[index + 1],  // Dịch tên ingredient
-                measure: translatedTexts[index + 1 + ingredients.length]  // Dịch measure
-            }));
-            console.log(translatedInstructions)
-            console.log(translatedIngredients)
-
-
+            // // Ánh xạ kết quả dịch lại vào đúng chỗ
+            // const translatedInstructions = translatedTexts[0];  // Bản dịch của `strInstructions`
+            // const translatedIngredients = ingredients.map((item, index) => ({
+            //     name: translatedTexts[index + 1],  // Dịch tên ingredient
+            //     measure: translatedTexts[index + 1 + ingredients.length]  // Dịch measure
+            // }));
+            // console.log(translatedInstructions)
+            // console.log(translatedIngredients)
 
 
-            const foundRecipe = await recipeModel.findOne({recipe_id_crawl: data[`idMeal`]})
-            if(foundRecipe) continue
-            const newRecipe = await recipeModel.create({
-                recipe_name: data["strMeal"],
-                recipe_category: data[`strCategory`],
-                recipe_desciption: translatedInstructions,
-                recipe_image: data[`strMealThumb`] ?  data[`strMealThumb`] : "",
-                recipe_id_crawl: data[`idMeal`],
-                recipe_ingredients: translatedIngredients,
-                recipe_youtube_url: data[`strYoutube`]? data[`strYoutube`]: ""
-            })
-            if(newRecipe) {
-                console.log(`Insert OK ${t}`)
-            } else {
-                console.log
-            }
+
+
+            // const foundRecipe = await recipeModel.findOne({recipe_id_crawl: data[`idMeal`]})
+            // if(foundRecipe) continue
+            // const newRecipe = await recipeModel.create({
+            //     recipe_name: data["strMeal"],
+            //     recipe_category: data[`strCategory`],
+            //     recipe_desciption: translatedInstructions,
+            //     recipe_image: data[`strMealThumb`] ?  data[`strMealThumb`] : "",
+            //     recipe_id_crawl: data[`idMeal`],
+            //     recipe_ingredients: translatedIngredients,
+            //     recipe_youtube_url: data[`strYoutube`]? data[`strYoutube`]: ""
+            // })
+            // if(newRecipe) {
+            //     console.log(`Insert OK ${t}`)
+            // } else {
+            //     console.log
+            // }
         } catch (error) {
             console.log(error)
         }
