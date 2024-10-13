992f48984f7a9f1b358639ed51c0e2e0c8cafa4c
naDuong2004 <nguyenanhduong112004@gmail.com>
2024-10-13 18:42:01 +0700

    sua loi

--- src/models/recipe.model.js
+++ src/models/recipe.model.js
@@ -10,31 +10,19 @@ const recipeSchema = new Schema(
     recipe_ingredients: [
       {
         name: { type: String, required: true },
-<<<<<<< HEAD
         measure: { type: String, default: "" },
-=======
-        measure: {type: String, default: ''}
->>>>>>> 1006e29014a3605a1e4e690c22f719aa3916db12
       },
     ],
     recipe_cook_time: { type: String, default: "" },
     recipe_youtube_url: {
-<<<<<<< HEAD
       type: String,
       default: "",
-=======
-      type: String, default: ""
->>>>>>> 1006e29014a3605a1e4e690c22f719aa3916db12
     },
     recipe_category: { type: String, default: "" },
     recipe_image: { type: String, default: "" },
     is_published: { type: Boolean, default: false },
     is_draft: { type: Boolean, default: true },
-<<<<<<< HEAD
     recipe_id_crawl: { type: String, required: true },
-=======
-    recipe_id_crawl: {type: String, required: true}
->>>>>>> 1006e29014a3605a1e4e690c22f719aa3916db12
   },
   {
     timestamps: true,
