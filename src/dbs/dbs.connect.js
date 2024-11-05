ceb6a793d6ea8a4bca1c7447cb2335d3ff4f31b0
naDuong2004 <nguyenanhduong112004@gmail.com>
2024-11-05 22:34:49 +0700

    add vercel.json

--- src/dbs/dbs.connect.js
+++ src/dbs/dbs.connect.js
@@ -2,11 +2,14 @@
 
 const { default: mongoose } = require("mongoose");
 
-//const connectString = process.env.DB_URL
-const connectString = process.env.DB_URL_TEST;
+const connectString = process.env.DB_URL;
+//const connectString = process.env.DB_URL_TEST;
 mongoose
   .connect(connectString)
   .then((_) => console.log("Connect MongoDB successfully!!"))
-  .catch((error) => console.log("Connect error!!"));
+  .catch((error) => {
+    console.log("Connect error!!");
+    console.error(error);
+  });
 
 module.exports = mongoose;
