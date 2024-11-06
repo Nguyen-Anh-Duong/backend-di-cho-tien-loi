8f6a36bddd9974b35afa4994b82003fd7833c42d
naDuong2004 <nguyenanhduong112004@gmail.com>
2024-11-06 11:25:25 +0700

    them bien moi truong

--- src/swagger.js
+++ src/swagger.js
@@ -2,9 +2,11 @@
 const swaggerUi = require("swagger-ui-express");
 const fs = require("fs");
 const YAML = require("yaml");
+const path = require("node:path");
+const swaggerPath = path.join(__dirname, "../swagger.yaml");
 
 const swagger = (app) => {
-  const file = fs.readFileSync("./swagger.yaml", "utf8");
+  const file = fs.readFileSync(swaggerPath, "utf8");
   const swaggerDocument = YAML.parse(file);
 
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
