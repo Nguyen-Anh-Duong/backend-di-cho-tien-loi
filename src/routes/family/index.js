f297d1ec6c2ce0bd865943cbab7d188168ad0caf
naDuong2004 <nguyenanhduong112004@gmail.com>
2024-12-21 17:33:10 +0700

    api family, shopping list

---
+++ src/routes/family/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const { asyncHandler } = require("../../helpers/asyncHandler");
+const { authentication } = require("../../auth/checkAuth");
+const FamilyController = require("../../controllers/family.controller");
+
+const router = express.Router();
+
+router.use(authentication);
+
+// router quan li thanh vien
+router.post("/join", asyncHandler(FamilyController.joinFamily));
+router.post("/leave/:familyId", asyncHandler(FamilyController.leaveFamily));
+
+router.delete(
+  "/:familyId/members",
+  asyncHandler(FamilyController.deleteMembers)
+);
+
+//router crud nhom
+router.get("/:familyId", asyncHandler(FamilyController.getFamilyInformation));
+//router.get("/", asyncHandler(FamilyController.getAllFamiliesJoined));
+
+router.post("/", asyncHandler(FamilyController.createNewFamily));
+
+router.patch(
+  "/:familyId",
+  asyncHandler(FamilyController.updateFamilyInformation)
+);
+router.delete("/:familyId", asyncHandler(FamilyController.deleteFamily));
+
+//router shopping list
+router.post(
+  "/:familyId/shopping-lists",
+  asyncHandler(FamilyController.createNewShoppingList)
+);
+
+router.patch(
+  "/:familyId/shopping-lists/:listId",
+  asyncHandler(FamilyController.updateShoppingListById)
+);
+
+router.delete(
+  "/:familyId/shopping-lists/:listId",
+  asyncHandler(FamilyController.deleteShoppingList)
+);
+
+module.exports = router;
