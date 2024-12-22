fe503a5fc6e35f2669154268b7dc3ab0403a658c
DUYCUTEPHOMAIQUE <dauhu1232019@gmail.com>
2024-12-23 03:24:28 +0700

    OK

--- src/firebase/firebaseAdmin.js
+++ src/firebase/firebaseAdmin.js
@@ -1,6 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../../serviceAccount.json'); // Đường dẫn đến tệp JSON
 admin.initializeApp({
- credential: admin.credential.cert(serviceAccount),}
-);
+    credential: admin.credential.applicationDefault(),
+  });
 module.exports = admin; 
